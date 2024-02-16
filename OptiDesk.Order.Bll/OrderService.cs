04a9f114be2d0f622ea1c02589c3fd2af584f581
Ludovic33Fr <l_lefebvre2001@yahoo.fr>
2024-02-16 02:41:49 +0100

    Added projet Order Bll and Dto

---
+++ OptiDesk.Order.Bll/OrderService.cs
@@ -0,0 +1,38 @@
+﻿using OptiDesk.Order.Dto;
+
+namespace OptiDesk.Order.Bll
+{
+    public class OrderService
+    {
+        public List<OrderUser> GetAllOrders() 
+        {
+            Random rnd = new Random();
+            List<OrderUser> res = new List<OrderUser>();
+            int orderId = 2000;
+            for (int i = 0; i < 200; i++)
+            {
+                res.Add(new OrderUser(orderId++, rnd.Next(1, 10),
+                    Guid.NewGuid().ToString(),
+                    DateTime.Now.AddDays(-rnd.Next(100)).AddMinutes(-rnd.Next(100))));
+            }
+
+            return res;
+        }
+
+        public OrderUser GetAllOrdersById(int id)
+        {
+            var allOrders = GetAllOrders();
+            var res = allOrders.First<OrderUser>(p => p.OrderId == id);
+
+            return res;
+        }
+
+        public List<OrderUser> GetAllOrdersByUserId(int userId)
+        {
+            var allOrders = GetAllOrders();
+            var res = allOrders.Where<OrderUser>(p => p.UserId == userId).ToList();
+
+            return res;
+        }
+    }
+}
---
+++ OptiDesk.Order.Dto/OrderUser.cs
@@ -0,0 +1,21 @@
+﻿namespace OptiDesk.Order.Dto
+{
+    public class OrderUser
+    {
+        public int UserId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public String ProductRef { get; set; }
+
+        public DateTime OrderTime { get; set; }
+
+        public OrderUser(int orderId, int userId, string productRef, DateTime orderTime)
+        {
+            OrderId = orderId;
+            UserId = userId;
+            ProductRef = productRef;
+            OrderTime = orderTime;
+        }   
+    }
+}
